04aa2c5cd31a7dfe3d5e5e3af5463e3f84df9aad
EniuK <semen1133@wp.pl>
2022-07-04 21:26:15 +0200

    add expand function

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 let clickpower = 1;
-let score = 5000;
-let money = 5000;
+let score = 50000;
+let money = 50000;
 let worker = 0;
-let expand = 0;
+let expand = 1.1;
+let expandCounter = 0;
 let factory = 0;
 let shop = 0;
 const multiplier = 1.1;
 let workerCost = 50;
 let shopCost = 300;
 let factoryCost = 1000;
+let expandCost = 5000;
+
+let moneyPerSec = 0;
 
 //main block
 const coin = document.body.querySelector("#mainClicker");
@@ -26,6 +30,10 @@ const shopAmount = document.querySelector("#shopAmount");
 const buyfactory = document.querySelector(".factory");
 const factorysCost = document.querySelector("#factoryCost");
 const factoryAmount = document.querySelector("#factoryAmount");
+//Expand functionality
+const buyExpand = document.querySelector(".expand");
+const expandsCost = document.querySelector("#expandCost");
+const expandAmount = document.querySelector("#expandAmount");
 
 const addToScoreAndMoney = function () {
   score = score + clickpower;
@@ -50,11 +58,21 @@ const buy = function (type, cost, queryCost, queryCounter, typeClickpower) {
   };
 };
 
-const buyExpand = function () {
-    
+const expandBuy = function () {
+  if (money > expandCost - 1) {
+    money = money - expandCost;
+    expandCounter++;
+    console.log(moneyPerSec);
+    moneyPerSec = moneyPerSec * expand;
+    expandCost = expandCost * 1.1;
+    expandsCost.innerHTML = expandCost;
+    expandAmount.innerHTML = expandCounter;
+    console.log("chuj");
+  }
 };
 
 //worker
+
 buyWorker.addEventListener(
   "click",
   buy(worker, workerCost, workersCost, workersAmount, 1)
@@ -63,7 +81,9 @@ buyWorker.addEventListener("click", () => {
   worker += 1;
   workerCost = Math.round(workerCost * multiplier);
 });
+
 //shop
+
 buyShop.addEventListener(
   "click",
   buy(shop, shopCost, shopsCost, shopAmount, 3)
@@ -72,7 +92,9 @@ buyShop.addEventListener("click", () => {
   shop += 1;
   shopCost = Math.round(shopCost * multiplier);
 });
+
 //factory
+
 buyfactory.addEventListener(
   "click",
   buy(factory, factoryCost, factorysCost, factoryAmount, 10)
@@ -82,14 +104,19 @@ buyfactory.addEventListener("click", () => {
   factoryCost = Math.round(factoryCost * multiplier);
 });
 
+//expand
+
+buyExpand.addEventListener("click", expandBuy);
+
 coin.addEventListener("click", addToScoreAndMoney);
 
 scoreAmount.innerHTML = score;
 moneyScore.innerHTML = money;
 
 setInterval(() => {
-  score = (score + worker * 1 + shop * 5 + factory * 30) ;
-  money = money + worker * 1 + shop * 5 + factory * 30;
+  score = score + worker * 1 + shop * 5 + factory * 30;
+  moneyPerSec = worker * 1 + shop * 5 + factory * 30;
+  money = money + moneyPerSec;
   scoreAmount.innerHTML = score;
   moneyScore.innerHTML = money;
 }, 1000);
