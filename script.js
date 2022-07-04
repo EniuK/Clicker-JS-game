7449c922c50f3d2ae0b163b9be8175501979d62e
EniuK <semen1133@wp.pl>
2022-07-04 19:44:55 +0200

    added one function to buy with parameters

--- script.js
+++ script.js
@@ -1,117 +1,95 @@
-
-
-let moneyValue = 'bronze'
-let clickpower = 1
-let score = 5000
-let money = 5000
-let worker = 0
-let expand = 1000
-let factory = 0
-let shop = 0
-const multipier = 1.1
-let workerCost = 50
-let shopCost = 300
-let factoryCost = 1000
+let clickpower = 1;
+let score = 5000;
+let money = 5000;
+let worker = 0;
+let expand = 0;
+let factory = 0;
+let shop = 0;
+const multiplier = 1.1;
+let workerCost = 50;
+let shopCost = 300;
+let factoryCost = 1000;
 
 //main block
-const coin = document.body.querySelector('#mainClicker')
-const scoreAmount = document.querySelector('#clicksPerSecond')
-const moneyScore = document.querySelector('#moneyAmount')
+const coin = document.body.querySelector("#mainClicker");
+const scoreAmount = document.querySelector("#clicksPerSecond");
+const moneyScore = document.querySelector("#moneyAmount");
 //worker functionality
-const workersAmount = document.querySelector('#workerAmount')
-const workersCost = document.querySelector('#workerCost')
-const buyWorker = document.querySelector('.worker')
+const workersAmount = document.querySelector("#workerAmount");
+const workersCost = document.querySelector("#workerCost");
+const buyWorker = document.querySelector(".worker");
 //shop functionality
-const buyShop = document.querySelector('.shop')
-const shopsCost = document.querySelector('#shopCost')
-const shopAmount = document.querySelector('#shopAmount')
+const buyShop = document.querySelector(".shop");
+const shopsCost = document.querySelector("#shopCost");
+const shopAmount = document.querySelector("#shopAmount");
 //factory functionality
-const buyfactory = document.querySelector('.factory')
-const factorysCost = document.querySelector('#factoryCost')
-const factoryAmount = document.querySelector('#factoryAmount')
+const buyfactory = document.querySelector(".factory");
+const factorysCost = document.querySelector("#factoryCost");
+const factoryAmount = document.querySelector("#factoryAmount");
 
-const addToScoreAndMoney = function()  {
-    score = score + clickpower
-    money = money + clickpower
-    scoreAmount.innerHTML = score
-    moneyScore.innerHTML = money
-     
-}
+const addToScoreAndMoney = function () {
+  score = score + clickpower;
+  money = money + clickpower;
+  scoreAmount.innerHTML = score;
+  moneyScore.innerHTML = money;
+};
 
-const workerBuy = function(){
-    if(money > workerCost -1){
-    worker = worker + 1
-    money = money - workerCost
-    workerCost = Math.round(workerCost*multipier)
-    workersCost.innerHTML = workerCost
-    workersAmount.innerHTML = worker
-    scoreAmount.innerHTML = score
-    moneyScore.innerHTML = money
-    clickpower = clickpower + 1
-    }else{
-        return
+const buy = function (type, cost, queryCost, queryCounter, typeClickpower) {
+  return function () {
+    if (money > cost - 1) {
+      type++;
+      const y = cost;
+      money = money - cost;
+      cost = Math.round(y * multiplier);
+      queryCost.innerHTML = cost;
+      queryCounter.innerHTML = type;
+      scoreAmount.innerHTML = score;
+      moneyScore.innerHTML = money;
+      clickpower = clickpower + typeClickpower;
     }
-}
+  };
+};
 
-const shopBuy = function(){
-    if(money > shopCost -1){
-    shop = shop + 1
-    money = money - shopCost
-    shopCost = Math.round(shopCost*multipier)
-    shopsCost.innerHTML = shopCost
-    shopAmount.innerHTML = shop
-    scoreAmount.innerHTML = score
-    moneyScore.innerHTML = money
-    clickpower = clickpower + 3
-    }else{
-        return
-    }
-}
+const buyExpand = function () {
+    
+};
 
-const factoryBuy = function(){
-    if(money > factoryCost -1){
-    factory = factory + 1
-    money = money - factoryCost
-    factoryCost = Math.round(factoryCost*multipier)
-    factorysCost.innerHTML = factoryCost
-    factoryAmount.innerHTML = factory
-    scoreAmount.innerHTML = score
-    moneyScore.innerHTML = money
-    clickpower = clickpower + 10
-    }else{
-        return
-    }
-}
-
-
+//worker
 buyWorker.addEventListener(
-    "click",
-    workerBuy
-)
-
+  "click",
+  buy(worker, workerCost, workersCost, workersAmount, 1)
+);
+buyWorker.addEventListener("click", () => {
+  worker += 1;
+  workerCost = Math.round(workerCost * multiplier);
+});
+//shop
 buyShop.addEventListener(
-    'click',
-    shopBuy
-)
-
+  "click",
+  buy(shop, shopCost, shopsCost, shopAmount, 3)
+);
+buyShop.addEventListener("click", () => {
+  shop += 1;
+  shopCost = Math.round(shopCost * multiplier);
+});
+//factory
 buyfactory.addEventListener(
-    'click',
-    factoryBuy
-)
+  "click",
+  buy(factory, factoryCost, factorysCost, factoryAmount, 10)
+);
+buyfactory.addEventListener("click", () => {
+  factory += 1;
+  factoryCost = Math.round(factoryCost * multiplier);
+});
 
-coin.addEventListener(
-    "click",
-    addToScoreAndMoney
-)
+coin.addEventListener("click", addToScoreAndMoney);
 
-
-
-scoreAmount.innerHTML = score
-moneyScore.innerHTML = money
+scoreAmount.innerHTML = score;
+moneyScore.innerHTML = money;
 
 setInterval(() => {
-    score = score + worker*1 + shop*5 + factory*30
-    money = money + worker*1 + shop*5 + factory*30
-    scoreAmount.innerHTML = score
-    moneyScore.innerHTML = money
+  score = (score + worker * 1 + shop * 5 + factory * 30) ;
+  money = money + worker * 1 + shop * 5 + factory * 30;
+  scoreAmount.innerHTML = score;
+  moneyScore.innerHTML = money;
 }, 1000);
